c166e7673a362b31cf96538bf06c203df9e44a88
salman <salman.allam@outlook.com>
2019-08-12 17:07:33 +1000

    fixed wishlist/profiles, removed subscription from them

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_12_063636) do
+ActiveRecord::Schema.define(version: 2019_08_12_070400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,6 @@ ActiveRecord::Schema.define(version: 2019_08_12_063636) do
     t.string "first_name"
     t.string "last_name"
     t.bigint "user_id"
-    t.string "subscription"
     t.integer "wishlist_counter"
     t.integer "swipe_counter"
     t.datetime "created_at", null: false
