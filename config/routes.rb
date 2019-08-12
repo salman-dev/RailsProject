59e57a12fbc22e6647a6dbde72ec12eecae36cb7
Phillip8232 <phillip8232@hotmail.com>
2019-08-12 12:29:24 +1000

    something changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   get 'contact/page'
   # get '/attractions/:id', to: 'attractions#index', as: 'attractions_index'
-  # get '/subscriptions' => 'subscription#index', 'as: :add_payment_method'
+  get '/subscriptions' => 'subscription#index', 'as: :add_payment_method'
 
   resources :attractions
   resources :locations
-  resources :subscriptions
+  resources :subscriptions  
   resources :profiles
   devise_for :users
   root 'landing#page'
